366c46840c56767e1f3fb51e27af5e9985f23ef4
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-29 17:25:08 +0530

    included tailwind and flowbite in project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/main/resources/templates/**/*.html', // For Thymeleaf
+    './src/main/resources/static/**/*.html',    // For static HTML
+    './src/main/resources/static/**/*.js'       // For JS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
