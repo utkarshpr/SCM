109077b474eaf47663887932ac3c02c46c96b8ec
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-29 13:52:49 +0530

    added sql db

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,19 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class PageController {
+    
+    @RequestMapping("/home")
+    public String Home(Model model){
+        System.out.println("Home Page Handler");
+        // sending data to view 
+        model.addAttribute("name", "Substring Tech");
+        return "home";
+    }
+}
