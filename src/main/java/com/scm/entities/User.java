89c137493ce2e035f2296e3d423deb0c60652468
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-30 15:37:58 +0530

    added entity as user , contact and social link

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,48 @@
+package com.scm.entities;
+
+import java.util.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="user")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String userId;
+    @Column(name = "user_name",nullable = false)
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+    @Column(length = 1000)
+    private String about;
+    @Column(length = 1000)
+    private String profilePic;
+    private String phoneNumber;
+    private boolean enable=false;
+    private boolean emailVerified=false;
+    private boolean phoneVerified=false;
+
+    //
+    private Providers provider=Providers.SELF;
+    private String providerUserId;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.LAZY,orphanRemoval = true)
+    private List<Contact> contacts=new ArrayList<>();
+   
+
+
+}
