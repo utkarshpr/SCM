fd8d4cc659f911bbf4aa14880b035e52d8672f22
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-12-01 09:24:59 +0530

    message added to alert with session control

--- src/main/java/com/scm/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/scm/controllers/GlobalExceptionHandler.java
@@ -1,9 +1,5 @@
 package com.scm.controllers;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
 // @ControllerAdvice
 // public class GlobalExceptionHandler {
 
--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,15 +1,11 @@
 package com.scm.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import com.scm.entities.Providers;
 import com.scm.entities.User;
 import com.scm.helper.Message;
