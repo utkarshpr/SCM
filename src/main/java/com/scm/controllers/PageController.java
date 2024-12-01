2d166af486503ce328617cfb5124761937af296e
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-12-01 09:40:13 +0530

    validation added with @valid BlindingResult

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,7 @@ package com.scm.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.scm.services.UserService;
 
 import forms.UserForm;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class PageController {
@@ -73,11 +75,15 @@ public class PageController {
 
     // processing register
     @RequestMapping(value = "/do-register", method = RequestMethod.POST)
-    public String processRegister(@ModelAttribute UserForm userForm, HttpSession session) {
+    public String processRegister(@Valid @ModelAttribute UserForm userForm,BindingResult rBindingResult, HttpSession session) {
         try {
             System.out.println("Processing registration");
             // fetch the Data from Form
             // System.out.println(userForm.toString());
+            //validation
+            if(rBindingResult.hasErrors()){
+              return "signup";
+            }
             User user = User.builder()
                     .name(userForm.getName())
                     .email(userForm.getEmail())
